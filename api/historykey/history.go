c91e1f005db2e306602f5c505b0da8bdc1984d29
Buglot <65818720+buglot@users.noreply.github.com>
2023-11-05 21:31:16 +0700

    new

--- api/historykey/history.go
+++ api/historykey/history.go
@@ -1,7 +1,6 @@
 package historykey
 
 import (
-	"API/Database"
 	"database/sql"
 
 	"github.com/gin-gonic/gin"
@@ -10,9 +9,7 @@ import (
 var HistoryDB *sql.DB
 
 func GetHistory(c *gin.Context) {
-	HistoryDB, _ = Database.GetDB()
 	codekey := c.DefaultQuery("codeKey", "")
-
 	rowHistory := c.DefaultQuery("row", "")
 	query := "select date , time ,report from history where mykey_codekey = ? order by idhistory desc limit ?"
 	row, err := HistoryDB.Query(query, codekey, rowHistory)
@@ -39,5 +36,5 @@ func GetHistory(c *gin.Context) {
 		dataHistory = append(dataHistory, rowData)
 	}
 	c.JSON(200, gin.H{"data": dataHistory})
-	HistoryDB.Close()
+	row.Close()
 }
